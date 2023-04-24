c5dff96596196cf61bb61c5b3c2647c4e140e0da
YourName0729 <qazwexsdc@gmail.com>
2023-04-24 20:28:37 +0800

    new trivial

---
+++ template/cpp.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define For(i, n) for (int i = 0; i < n; i++)
+#define Forcase int __t = 0; cin >> __t; while (__t--)
+#define pb push_back
+#define ll long long
+#define ull unsigned long long
+#define ar array
+
+using namespace std;
+
+const int MOD = 1e9 + 7;
+const int INF = 2147483647;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+    return 0;
+}
